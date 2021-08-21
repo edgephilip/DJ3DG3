6c3f1d64ebaea72918d8b10e05630eedc3a16fa1
edgephilip <56555041+edgephilip@users.noreply.github.com>
2021-08-21 10:45:42 -0500

    Add files via upload

---
+++ web-ui/public/config.js
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+// Amazon IVS Playback URL
+// Replace this with your own Amazon IVS Playback URL
+export const PLAYBACK_URL = "https://89867222085c.us-west-2.playback.live-video.net/api/video/v1/us-west-2.470224751209.channel.TsLr48WlDSW8.m3u8";
+
+// Chat websocket address
+export const CHAT_WEBSOCKET = "wss://52alibz4ul.execute-api.us-west-2.amazonaws.com/Prod";
